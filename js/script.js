2d7fa02d286e407a95b86ea6b3ad4e93455b4e9e
Altug Cakmakci <altugcakmakci@gmail.com>
2021-10-01 16:11:30 -0400

    links added

---
+++ js/script.js
@@ -0,0 +1,9 @@
+let project1 = document.getElementById("project1");
+let project2 = document.getElementById("project2");
+let project3 = document.getElementById("project3");
+let project4 = document.getElementById("project4");
+
+project1.addEventListener("click", function(event){document.location = 'https://altugcakmakci.github.io/weather_dashboard'});
+project2.addEventListener("click", function(event){document.location = 'https://altugcakmakci.github.io/day_planner'});
+project3.addEventListener("click", function(event){document.location = 'https://altugcakmakci.github.io/rpsGame'});
+project4.addEventListener("click", function(event){document.location = 'https://altugcakmakci.github.io/movie_pal'});
